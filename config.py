b96240c59b7c3557701069da2f98db6e035a32e7
sabarirko <59568865+sabarirko@users.noreply.github.com>
2025-09-28 12:57:42 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+[DEFAULT]
+# Account name
+account =projectsg16
+# Azure Storage account access key
+key =10cthiqNfjVNv0UpJkGY3X/Q4ouBd9hWxCjfWpkq0/SrUTSCoXHOfgHOlCrzDhoqQZf3HqlAAagF+AStYblMWw==
+# Container name
+container =upload
